5b00d11aace78e45d63c8e272ca870b806d8b04a
aravinthRam <aravinth1991@gmail.com>
2015-09-08 17:15:26 +0530

    index added to home page

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -17,7 +17,7 @@
 
   <nav class="top-menu">
     <div class="nav-wrapper mat-clr">
-      <a href="#!" class="brand-logo"><img src="{{asset('inshorts/img/logo.png')}}"></a>
+      <a href="{{route('home')}}" class="brand-logo"><img src="{{asset('inshorts/img/logo.png')}}"></a>
       <!-- <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a> -->
      
       <ul class="right">  
