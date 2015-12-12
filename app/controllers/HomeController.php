2109b38bd66aa30ff844c9028cf4b6e3c2a31ac2
ramnethaji <ram@appoets.com>
2015-12-12 19:20:41 +0530

    Add publisher icon and css

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -225,6 +225,10 @@ class HomeController extends BaseController {
 		              </div>
 		              <div class="card-content">
 		               <p class="text-justify">'.$post->des.'</p>
+		               		<div class="au-btm">
+			               		<div class="au-left"><img src="https://scontent.fmaa1-2.fna.fbcdn.net/hphotos-xpf1/v/t1.0-9/12043110_834950396603352_6113846222889579456_n.jpg?oh=a34ae0818f89f3408852824b831c8f59&amp;oe=571FC22E"></div>
+			               		
+			               	</div>
 		              </div>
 
 		              <div class="card-action text-center">
@@ -268,6 +272,10 @@ class HomeController extends BaseController {
 		              </div>
 		              <div class="card-content">
 		               <p class="text-justify">'.$post->des.'</p>
+		               <div class="au-btm">
+			               		<div class="au-left"><img src="https://scontent.fmaa1-2.fna.fbcdn.net/hphotos-xpf1/v/t1.0-9/12043110_834950396603352_6113846222889579456_n.jpg?oh=a34ae0818f89f3408852824b831c8f59&amp;oe=571FC22E"></div>
+			               		
+			               	</div>
 		              </div>
 
 		              <div class="card-action text-center">
