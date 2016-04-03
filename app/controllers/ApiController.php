f9c3bbf7fd84f74bc7ad936e321c95d9a78d6db7
amrutjadhav <amrutjadhav@users.noreply.github.com>
2016-04-03 12:41:36 +0530

    Fetch New news only
    Controller updated to fetch only recent news greater than a provided id.

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -122,6 +122,100 @@ class ApiController extends \BaseController
 		}
 		return Response::json($response_array);
 	}
+	
+	
+	public function getNewPostCat()
+	{
+		$cat = Input::get('category');
+		$take = Input::get('take');
+		$skip = Input::get('skip');
+		$postid=Input::get('postid');
+
+		$validator = Validator::make(
+			array(
+				'take' => $take,
+				'skip' => $skip,
+				'postid' =>$postid,
+			), array(
+				'take' => 'required|integer',
+				'skip' => 'required|integer',
+				'postid' => 'required|integer',
+			)
+		);
+
+		if ($validator->fails()) {
+			$error_messages = $validator->messages()->all();
+			$response_array = array('success' => false, 'error' => 'Invalid Input', 'error_code' => 401, 'error_messages' => $error_messages);
+			$response_code = 200;
+		} else {
+			if (!$cat) {
+				$posts = Post::where('is_approved', 1)->where('id','>', $postid)->take($take)->skip($skip)->orderBy('created_at', 'desc')->get();
+				$counts = Post::where('is_approved', 1)->count();
+				$datas = array();
+				foreach ($posts as $post) {
+
+					if($publisher = Publisher::find($post->publisher_id)) {
+						$publisher_name = $publisher->name;
+						$publisher_image = $publisher->image;
+					} else {
+						$publisher_name = "";
+						$publisher_image = "";
+					}
+
+					$cat_name = $post->share_cat;
+					$link = URL::to('/'). '/read/' . $cat_name . '/' . $post->link;
+					$data = array();
+					$data['id'] = $post->id;
+					$data['title'] = $post->title;
+					$data['description'] = $post->des;
+					$data['url'] = $post->url;
+					$data['image'] = $post->image;
+					$data['time'] = $post->created_at->diffForHumans();
+					$data['share_link'] = $link;
+					$data['publisher_image'] = $publisher_image;
+					$data['publisher_name'] = $publisher_name;
+
+					array_push($datas, $data);
+				}
+				$response_array = array('success' => true, 'posts' => $datas, 'counts' => $counts);
+			} else {
+
+				$postss = Post::where('is_approved', 1)->where('category', 'like', '%' . $cat . '%')->where('id','>', $postid)->take($take)->skip($skip)->orderBy('created_at', 'desc')->get();
+				$counts = Post::where('is_approved', 1)->where('category', 'like', '%' . $cat . '%')->count();
+				$datas = array();
+				foreach ($postss as $post) {
+
+					if($publisher = Publisher::find($post->publisher_id)) {
+						$publisher_name = $publisher->name;
+						$publisher_image = $publisher->image;
+					} else {
+						$publisher_name = "";
+						$publisher_image = "";
+					}
+
+					$cat_name = $post->share_cat;
+					$link = URL::to('/') . '/read/' . $cat_name . '/' . $post->link;
+					$data = array();
+					$data['id'] = $post->id;
+					$data['title'] = $post->title;
+					$data['description'] = $post->des;
+					$data['url'] = $post->url;
+					$data['image'] = $post->image;
+					$data['time'] = $post->created_at->diffForHumans();
+					$data['share_link'] = $link;
+
+					$data['publisher_image'] = $publisher_image;
+					$data['publisher_name'] = $publisher_name;
+					
+
+					array_push($datas, $data);
+				}
+				$response_array = array('success' => true, 'posts' => $datas, 'counts' => $counts);
+			}
+		}
+		return Response::json($response_array);
+	}
+
 
 
 	public function register()
